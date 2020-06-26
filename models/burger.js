caa23129d1e00d1ad28ea37cfafbeab2e9e69008
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-25 21:15:41 -0600

    fixing errors

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 
 var router = express.Router();
-var burger = require('../mopdels/burger.js');
+var burger = require('../models/burger.js');
 
 // get rout -> index
 router.get('/', function(req, res) {
